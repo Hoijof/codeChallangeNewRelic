714dbd4360172a369a88eead1a4640c75aa1796b
Humberto Gómez <humberto.gomez@basetis.com>
2017-05-05 17:37:17 +0200

    Improve test

--- test/classes/Host.spec.js
+++ test/classes/Host.spec.js
@@ -13,13 +13,14 @@ describe('Host Prototype', function () {
     expect(this.host.name).toBe(TEST_NAME);
   });
 
-  it('should add an app', function () {
+  // Adding
+  it('should add an app when the host is empty', function () {
     let app = helper.host.addApp(fakeData[0], this.host);
 
     expect(this.host.firstApp).toEqual(app);
   });
 
-  it('should add two apps and order them', function () {
+  it('should add two apps and order them with an empty host', function () {
     let app = helper.host.addApp(fakeData[1], this.host),
       app2 = helper.host.addApp(fakeData[0], this.host);
 
@@ -27,6 +28,37 @@ describe('Host Prototype', function () {
     expect(this.host.firstApp.links[this.host.name].nextApp).toEqual(app);
   });
 
+  it('should add an app that will be the first app and keep the structure coherent', function () {
+    let app = helper.host.addApp(fakeData[1], this.host),
+      app3 = helper.host.addApp(fakeData[2], this.host),
+      app2 = helper.host.addApp(fakeData[0], this.host);
+
+    expect(this.host.firstApp).toEqual(app2);
+    expect(this.host.firstApp.links[this.host.name].nextApp).toEqual(app3);
+    expect(this.host.firstApp.links[this.host.name].nextApp.links[this.host.name].nextApp).toEqual(app);
+  });
+
+  it('should add an app that will go in the middle and keep the structure coherent', function () {
+    let app = helper.host.addApp(fakeData[1], this.host),
+      app2 = helper.host.addApp(fakeData[0], this.host),
+      app3 = helper.host.addApp(fakeData[2], this.host);
+
+    expect(this.host.firstApp).toEqual(app2);
+    expect(this.host.firstApp.links[this.host.name].nextApp).toEqual(app3);
+    expect(this.host.firstApp.links[this.host.name].nextApp.links[this.host.name].nextApp).toEqual(app);
+  });
+
+  it('should add an app that will go in last position and keep the structure coherent', function () {
+    let app3 = helper.host.addApp(fakeData[2], this.host),
+      app2 = helper.host.addApp(fakeData[0], this.host),
+      app = helper.host.addApp(fakeData[1], this.host);
+
+    expect(this.host.firstApp).toEqual(app2);
+    expect(this.host.firstApp.links[this.host.name].nextApp).toEqual(app3);
+    expect(this.host.firstApp.links[this.host.name].nextApp.links[this.host.name].nextApp).toEqual(app);
+  });
+
+  // Removing
   it('should remove an app when there\'s only one left successfully', function () {
     let app = helper.host.addApp(fakeData[1], this.host);
 
@@ -49,7 +81,7 @@ describe('Host Prototype', function () {
     }
   });
 
-  it('should remove the first app and keep the strucutre coherent', function () {
+  it('should remove the first app and keep the structure coherent', function () {
     let app = helper.host.addApp(fakeData[1], this.host),
       app2 = helper.host.addApp(fakeData[0], this.host),
       app3 = helper.host.addApp(fakeData[2], this.host);
@@ -65,7 +97,7 @@ describe('Host Prototype', function () {
 
   });
 
-  it('should remove an app in between and keep the strucutre coherent', function () {
+  it('should remove an app in between and keep the structure coherent', function () {
     let app = helper.host.addApp(fakeData[1], this.host),
       app2 = helper.host.addApp(fakeData[0], this.host),
       app3 = helper.host.addApp(fakeData[2], this.host);
@@ -81,7 +113,7 @@ describe('Host Prototype', function () {
 
   });
 
-  it('should remove the last app and keep the strucutre coherent', function () {
+  it('should remove the last app and keep the structure coherent', function () {
     let app = helper.host.addApp(fakeData[1], this.host),
       app2 = helper.host.addApp(fakeData[0], this.host),
       app3 = helper.host.addApp(fakeData[2], this.host);
